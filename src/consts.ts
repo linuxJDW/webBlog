a6c4bfa130c5218f9e6d949531b93ab9adcdcdc6
linuxJDW <linuxJDW@163.com>
2023-03-20 17:17:29 +0800

    添加http

--- src/consts.ts
+++ src/consts.ts
@@ -64,12 +64,19 @@ export const SIDEBAR: Sidebar = {
 			{ text: 'STM32F103C8T6', link: 'demo/demo' },
 		],
 		'IOT': [{ text: '物联网导论', link: 'demo/demo' }],
+		'http': [
+			{ text: 'http介绍', link: 'demo/demo' },
+			{ text: 'libcurl错误码', link: 'http/libcurl错误码' },
+			{ text: 'demo', link: 'demo/demo' },
+			{ text: 'demo', link: 'demo/demo' },
+			{ text: 'demo', link: 'demo/demo' },
+		],
 		'知识拓展': [
 			{ text: '友情链接', link: 'introduce/友情链接' },
 			{ text: '公司介绍', link: 'introduce/公司介绍' },
 			{ text: '数据报告', link: 'introduce/数据报告' },
-			{ text: '关于Tcube', link: 'introduce/关于Tcube' },
+			{ text: '关于Tcube', link: 'introduce/关于我们' },
 			{ text: '商业合作', link: 'introduce/商业合作' },
 		],
-	},
+	}
 };
--- src/content/docs/introduce/关于Tcube.md
+++
@@ -1,7 +0,0 @@
----
-title: "关于Tcube"
-description: "Lorem ipsum dolor sit amet - 4"
----
-
-***
-我也只是个demo 啊！
